3830466b39bcd012479e150c0c9de5568eada8d8
Andrew <andrew@swiftium.com>
2016-09-20 02:59:48 -0400

    :tada: Init Commit

---
+++ Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AdbTouchGenerator
+{
+    class Program
+    {
+        const string EVENT_LOCATOR = "/dev/input/event2";
+        const string POINT_LOCATOR = "ABS_MT_POSITION_";
+        const string TOUCH_LOCATOR = "BTN_TOUCH";
+
+        static void Main(string[] args)
+        {
+            string _filename = @"E:\GitHub\Projects\Device Loader\SwiftiumDeviceLoaders\bin\Debug\platform-tools\touches.txt", _deviceId = null, _outputFile = null;
+
+            if (args.Length > 0)
+            {
+                _filename = args[0];
+
+                if (args.Length >= 2)
+                    _deviceId = args[1];
+
+                if (args.Length >= 3)
+                    _outputFile = args[2];
+            }
+
+            if (System.IO.File.Exists(_filename))
+            {
+                // Only Up, Down and positions of touches
+                IEnumerable<string> _lines = System.IO.File.ReadAllLines(_filename, Encoding.UTF8).Where(l => l.IndexOf(EVENT_LOCATOR) > -1 && (l.IndexOf(TOUCH_LOCATOR) > -1 || l.IndexOf(POINT_LOCATOR) > -1));
+
+                List<AdbEvent> _es = new List<AdbEvent>();
+                AdbEvent _e = null;
+                int _x = -1, _y = -1;
+
+                foreach (string _l in _lines)
+                {
+                    string[] _p = null;
+
+                    if (_l.IndexOf(TOUCH_LOCATOR) > -1)
+                    {
+                        // p[0] = /dev/input/event2: EV_KEY
+                        // P[1] = BTN_TOUCH
+                        // P[2] = DOWN
+
+                        //if (_es.Count > 1)
+                        //{
+                        //    if (_x > -1 && _es[_es.Count - 1]._points.Count > 0)
+                        //    {
+                        //        _es[_es.Count - 1]._points.Add(new AdbEvent.AdbPointEvent() { _point = new Point(_x, _es[_es.Count - 1]._points[_es[_es.Count - 1]._points.Count - 1]._point.Y), _timestamp = -1 });
+                        //        _x = -1;
+                        //    }
+                        //}
+
+                        _p = _l.Split(new string[] { "       ", "            " }, StringSplitOptions.None);
+
+                        if (_p[2].IndexOf("DOWN") > -1)
+                            _e = new AdbEvent();
+                        else if (_p[2].IndexOf("UP") > -1 && _e != null) {
+                            _es.Add(_e);
+                            _e = null;
+                        }
+                    }
+                    else if (_l.IndexOf(POINT_LOCATOR) > -1)
+                    {
+                        // p[0] = /dev/input/event2: EV_ABS
+                        // P[1] = ABS_MT_POSITION_X
+                        // P[2] = 00000114
+
+                        _p = _l.Split(new string[] { "       ", "    " }, StringSplitOptions.None);
+
+                        if (_e != null)
+                        {
+                            if(_p[2].IndexOf("X") > -1)
+                                _x =  Convert.ToInt32(_p[3].Replace(" ", ""), 16);
+                            else if (_p[2].IndexOf("Y") > -1)
+                                _y = Convert.ToInt32(_p[3].Replace(" ", ""), 16);
+
+                            // Ignore this point because it is a single
+                            if (_y > -1 && _x == -1)
+                                _y = -1;
+
+                            if (_x > -1 && _y > -1)
+                            {
+                                _e._points.Add(new AdbEvent.AdbPointEvent() { 
+                                    _point = new Point(_x, _y), 
+                                    _timestamp = decimal.Parse(_l.Substring(_l.IndexOf("[") + 1, (_l.IndexOf("]") - _l.IndexOf("[")) - 2).Replace(" ", ""))
+                                });
+                                _x = -1;
+                                _y = -1;
+                            }
+                        }
+                    }
+                }
+
+                StringBuilder _sb = new StringBuilder();
+
+                foreach (AdbEvent _ae in _es)
+                {
+                    if (_ae._points.Count > 0)
+                    {
+                        _sb.AppendLine(_deviceId != null ? _ae.GetAdbSendCommand(_deviceId) : _ae.GetAdbSendCommand());
+                        _sb.AppendLine("ping 192.0.2.1 -n 1 -w 250 >nul");
+                    }
+                }
+
+                System.IO.File.WriteAllText(_outputFile != null ? _outputFile : "output.bat", _sb.ToString());
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+
+            Console.ReadKey();
+        }
+
+        private class AdbEvent
+        {
+            public class AdbPointEvent
+            {
+                public Point _point { get; set; }
+                public decimal _timestamp { get; set; }
+            }
+
+            public List<AdbPointEvent> _points { get; set; }
+
+            public AdbEvent()
+            {
+                this._points = new List<AdbPointEvent>();
+            }
+
+            private bool _isSwipe()
+            {
+                if (this._points.Count > 1)
+                {
+                    int _maxDistanceX = Math.Abs(_points.Max(p => p._point.X) - _points.Min(p => p._point.X)),
+                        _maxDistanceY = Math.Abs(_points.Max(p => p._point.Y) - _points.Min(p => p._point.Y));
+
+                    if (_maxDistanceX > 75 || _maxDistanceY > 75)
+                        return true;
+                }
+                return false;
+            }
+
+            public string GetAdbSendCommand()
+            {
+                if (!this._isSwipe())
+                {
+                    int _averageX = this._points[0]._point.X, //(this._points.Sum(p => p._point.X) / this._points.Count),
+                        _averageY = this._points[0]._point.Y; //(this._points.Sum(p => p._point.Y) / this._points.Count);
+
+                    return string.Format("adb shell input tap {0} {1}", _averageX, _averageY);
+                }
+                else
+                {
+                    return string.Format("adb shell input swipe {0} {1} {2} {3} {4}", this._points[0]._point.X, this._points[0]._point.Y, this._points[this._points.Count - 1]._point.X, this._points[this._points.Count - 1]._point.Y + 100, 1300);
+                    // (int)((this._points[this._points.Count - 1]._timestamp - this._points[0]._timestamp) * 1000)
+                }
+            }
+
+            public string GetAdbSendCommand(string deviceId)
+            {
+                if (!this._isSwipe())
+                {
+                    int _averageX = (this._points.Sum(p => p._point.X) / this._points.Count),
+                        _averageY = (this._points.Sum(p => p._point.Y) / this._points.Count);
+
+                    return string.Format("adb -s {0} shell input tap {1} {2}", deviceId, _averageX, _averageY);
+                }
+                else
+                {
+                    return string.Format("adb -s {0} shell input swipe {1} {2} {3} {4} {5}", deviceId, this._points[0]._point.X, this._points[0]._point.Y, this._points[this._points.Count - 1]._point.X, this._points[this._points.Count - 1]._point.Y + 100, 1300);
+                    // (int)((this._points[this._points.Count - 1]._timestamp - this._points[0]._timestamp) * 1000)
+                }
+            }
+        }
+    }
+}
---
+++ bin/Debug/output.bat
@@ -0,0 +1,230 @@
+adb shell input tap 741 447
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 404 512
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 401 567
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 814 1170
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 92 172
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 743 780
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 380 557
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 846 1154
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 102 170
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 724 1189
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 428 592
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 801 1131
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 77 171
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 734 1763 732 581 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 675 1072
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 445 568
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 891 1154
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 79 185
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 758 1691 775 514 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 739 981
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 370 557
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 837 1115
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 94 166
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 735 1763 738 663 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 727 683
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 386 585
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 870 1147
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 94 154
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 697 1082
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 361 547
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 806 1158
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 106 169
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 736 1618
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 403 548
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 872 1143
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 116 163
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 593 1338
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 437 615
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 851 1145
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 83 169
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 762 1574 786 618 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 766 837
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 277 585
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 849 1117
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 106 186
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 817 1042
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 390 652
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 844 1143
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 130 173
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 882 1242
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 400 611
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 877 1106
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 130 169
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 853 1471
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 278 592
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 854 1152
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 120 171
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 877 1662
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 371 659
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 90 163
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 393 572
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 867 1094
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 110 164
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 808 1600 811 583 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 747 1255
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 372 571
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 900 1137
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 99 192
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 737 1495
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 357 619
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 877 1163
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 89 192
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 815 1701 796 643 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 736 1683
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 418 598
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 911 1162
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 115 169
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 719 1631 760 418 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 753 881
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 354 600
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 887 1108
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 143 195
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 747 1586 772 490 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 778 792
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 396 570
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 903 1171
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 110 197
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 780 1264
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 359 583
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 872 1139
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 124 188
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 834 1070
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 339 619
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 857 1136
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 98 177
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 764 1686 805 664 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 777 897
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 412 563
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 870 1152
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 109 182
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 802 1151
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 326 609
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 888 1149
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 98 191
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 805 1723 858 614 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 749 1299
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 401 593
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 843 1160
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 122 177
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input swipe 796 1645 838 610 1300
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 641 1755
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 400 511
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 384 568
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 817 1109
+ping 192.0.2.1 -n 1 -w 250 >nul
+adb shell input tap 112 175
+ping 192.0.2.1 -n 1 -w 250 >nul
